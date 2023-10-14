2e6100ac415bb72b7c4130b3ee151705bea63426
Franco Jalil <111142067+FrancoJalil@users.noreply.github.com>
2023-10-13 22:40:16 -0300

    Update redirects.sh

--- redirects.sh
+++ redirects.sh
@@ -3,9 +3,9 @@
 # Crea un archivo de configuración de redirecciones vacío
 > /etc/nginx/conf.d/redirects.conf
 
-# Agrega redirecciones para quitar la extensión ".html"
-echo "location /my-generations { return 301 /my-generations.html; }" >> /etc/nginx/conf.d/redirects.conf
-echo "location /profile { return 301 /profile.html; }" >> /etc/nginx/conf.d/redirects.conf
-# Agrega más redirecciones según tus páginas
+# Agrega redirecciones utilizando las variables de entorno
+echo "location $PAGE1_PATH { return 301 $PAGE1_PATH.html; }" >> /etc/nginx/conf.d/redirects.conf
+echo "location $PAGE2_PATH { return 301 $PAGE2_PATH.html; }" >> /etc/nginx/conf.d/redirects.conf
+# Agrega más redirecciones utilizando variables de entorno
 
 exec "$@"
